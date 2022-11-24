a06986090ef67ee6cb9258ea592f51feeefea63b
freekmurze <freekmurze@users.noreply.github.com>
2022-11-24 16:44:52 +0000

    Fix styling

--- tests/MailcoachMailerTest.php
+++ tests/MailcoachMailerTest.php
@@ -5,30 +5,30 @@ use Illuminate\Support\Facades\Mail;
 use Spatie\MailcoachMailer\Exceptions\InvalidMailerConfig;
 use Spatie\MailcoachMailer\MailcoachApiTransport;
 
-beforeEach(function() {
-   config()->set('mail.default', 'mailcoach');
+beforeEach(function () {
+    config()->set('mail.default', 'mailcoach');
 
-   config()->set('mail.mailers.mailcoach', [
-       'transport' => 'mailcoach',
-       'domain' => 'fake-domain',
-       'token' => 'my-fake-token',
-   ]);
+    config()->set('mail.mailers.mailcoach', [
+        'transport' => 'mailcoach',
+        'domain' => 'fake-domain',
+        'token' => 'my-fake-token',
+    ]);
 });
 
-it('can register a transport', function() {
-   $mailer = Mail::mailer('mailcoach');
+it('can register a transport', function () {
+    $mailer = Mail::mailer('mailcoach');
 
-   expect($mailer)->toBeInstanceOf(Mailer::class);
+    expect($mailer)->toBeInstanceOf(Mailer::class);
 
-   /** @var MailcoachApiTransport $transport */
-   $transport = $mailer->getSymfonyTransport();
+    /** @var MailcoachApiTransport $transport */
+    $transport = $mailer->getSymfonyTransport();
 
-   expect($transport)->toBeInstanceOf(MailcoachApiTransport::class);
+    expect($transport)->toBeInstanceOf(MailcoachApiTransport::class);
 
-   expect(invade($transport))->apiToken->toBe('my-fake-token');
+    expect(invade($transport))->apiToken->toBe('my-fake-token');
 });
 
-it('will throw an exception when a required config key is not set', function(string $key) {
+it('will throw an exception when a required config key is not set', function (string $key) {
     config()->set($key, null);
 
     Mail::mailer('mailcoach');
