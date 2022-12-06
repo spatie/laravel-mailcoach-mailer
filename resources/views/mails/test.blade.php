2fdbb6a4c559c5494637a69a34a63c046e953576
Freek Van der Herten <freek@spatie.be>
2022-12-06 14:05:35 +0100

    wip

--- resources/views/mails/test.blade.php
+++ resources/views/mails/test.blade.php
@@ -3,11 +3,11 @@
 
 You can now send transactional mails using Mailcoach.
 
-To learn all the options, read [the docs on sending transactional mails in Laravel](https://mailcoach.app/docs/cloud/using-mailcoach/transactional-mails/using-laravel).
+To learn all the options, read [the docs on sending transactional emails in Laravel](https://mailcoach.app/docs/cloud/using-mailcoach/transactional-mails/using-laravel).
 
-Mailcoach can do more than just send transactional mails. It is a complete email marketing platform that covers all your email needs for a fraction of the cost. Mailcoach can send email campaigns to lists of any size. We also offer best-in-class email automation, so you can easily create drip campaigns and more.
+Mailcoach can do much more besides sending transactional emails. It is a complete email marketing platform that covers all your email needs for a fraction of the cost. Mailcoach can send email campaigns to lists of any size. We also offer best-in-class email automation, so you can easily create drip campaigns and more.
 
-Mailcoach is the most affordable email solution. Most of our competitors price by the size of your email list, we price by the amount of mails sent.
+Mailcoach is the most affordable email solution. Most of our competitors price by the size of your email list; we price by the number of mails sent.
 
 <x-mail::button url="https://mailcoach.app">
     Discover Mailcoach
